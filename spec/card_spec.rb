7e63f17951b7faa0f12cfdfbae46748cb407a5a2
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2020-11-09 20:24:53 -0800

    added tests for valid amex and visa cards

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,13 +8,47 @@ RSpec.describe "a credit card" do
     expect(new_card.card_number).to eq("1234567890") 
   end
 
-  context "valid cards" do
+  context "valid visa cards" do
 
     it "validates visa card" do
       new_card = Card.new("4242424242424242")
       expect(new_card.card_valid).to be true
     end
 
+    it "validates visa card with dashes" do
+      new_card = Card.new("4242-4242-4242-4242")
+      expect(new_card.card_valid).to be true
+    end
 
+
+    it "validates visa card with spaces" do
+      new_card = Card.new("4242 4242 4242 4242")
+      expect(new_card.card_valid).to be true
+    end
+
+    it "validates visa card" do
+      new_card = Card.new("4242424242424242")
+      expect(new_card.card_valid).to be true
+    end
   end
+
+  context "valid amex cards" do
+
+    it "validates amex card" do
+      new_card = Card.new("378282246310005")
+      expect(new_card.card_valid).to be true
+    end
+
+    it "validates amex card with dashes" do
+      new_card = Card.new("3782-822463-10005")
+      expect(new_card.card_valid).to be true
+    end
+
+
+    it "validates amex card with spaces" do
+      new_card = Card.new("3782 822463 10005")
+      expect(new_card.card_valid).to be true
+    end
+  end
+
 end
